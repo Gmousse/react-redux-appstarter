dd34907227252452a55575b319f32d5e353a0957
Guillaume Mousnier <mousnier.guillaume@gmail.com>
2016-01-09 22:02:47 +0100

    Add example reducers

--- src/reducers.js
+++ src/reducers.js
@@ -1,87 +1,20 @@
 import { combineReducers } from 'redux';
 import React from 'react';
-import { HANDLE_SEARCH, HANDLE_RESET, WAITING_VIGNETTES, RECEIVING_VIGNETTES,
-     FILTER_VIGNETTES, DISPLAY_DOCUMENT, DOWNLOAD_DOCUMENT } from './actions.js';
-
-// Utils
-
-function textToTags(text) {
-    const tags = new Set();
-    text.split(new RegExp('[ ,;]', 'g')).forEach((tag) => {
-        if (tag.length > 1) {
-            tags.add(tag);
-        }
-    });
-    return Array.from(tags);
-}
-
-function filterArrayByWords(arrayToFilter, filters) {
-    const filteredArray = new Set();
-    for (const filter of filters) {
-        filteredArray.add(arrayToFilter.find((element) => element.includes(filter)));
-    }
-    filteredArray.delete(undefined);
-    return Array.from(filteredArray);
-}
+import { ACTION_TYPE } from './actions.js';
 
 // Initial States
 
-const searchInitialState = {
-    text: '',
-    tagsList: [],
-};
-
-const vignettesInitialState = {
-    list: [],
-    listToDisplay: [],
-};
-
-const documentToDisplayInitialState = {
-    content: undefined,
+const yourInitialState = {
+    test: '',
 };
 
 // Stores
 
-export default function search(state = searchInitialState, action) {
+export default function yourState(state = yourInitialState, action) {
     switch (action.type) {
-    case HANDLE_SEARCH:
+    case ACTION_TYPE:
         return Object.assign({}, state, {
-            text: action.newSearchText,
-            tagsList: textToTags(action.newSearchText),
-        });
-    case HANDLE_RESET:
-        return searchInitialState;
-    default:
-        return state;
-    }
-}
-
-export default function vignettes(state = vignettesInitialState, action) {
-    switch (action.type) {
-    case WAITING_VIGNETTES:
-        return state;
-    case RECEIVING_VIGNETTES:
-        return Object.assign({}, state, {
-            list: action.vignettesList,
-        });
-    case FILTER_VIGNETTES:
-        return Object.assign({}, state, {
-            listToDisplay: filterArrayByWords(state.list, action.filters),
-        });
-    default:
-        return state;
-    }
-}
-
-export default function documentToDisplay(state = documentToDisplayInitialState, action) {
-    switch (action.type) {
-    case DISPLAY_DOCUMENT:
-        return Object.assign({}, state, {
-            content: <object data={action.apiRequest}/>,
-        });
-    case DOWNLOAD_DOCUMENT:
-        return Object.assign({}, state, {
-            content: <a href={action.apiRequest} download/>,
+            test: 'Hello World',
         });
     default:
         return state;
@@ -89,9 +22,7 @@ export default function documentToDisplay(state = documentToDisplayInitialState,
 }
 
 const store = combineReducers({
-    search,
-    vignettes,
-    documentToDisplay,
+    yourState,
 });
 
 export default store;
