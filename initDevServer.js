a8ea89536705208796f91be07692cbe320ffd9ba
Guillaume Mousnier <mousnier.guillaume@gmail.com>
2016-01-12 23:45:28 +0100

    Just update initDevServer.js

--- initDevServer.js
+++ initDevServer.js
@@ -1,26 +1,31 @@
-var path = require('path');
-var express = require('express');
-var webpack = require('webpack');
-var config = require('./webpack.config');
+'use strict';
+const path = require('path');
+const express = require('express');
+const webpack = require('webpack');
+const config = require('./webpack.config');
 
-var app = express();
-var compiler = webpack(config);
+const app = express();
+if (process.argv[2]) {
+    config.entry.push(process.argv[2]);
+}
+const compiler = webpack(config);
+
 app.use(require('webpack-dev-middleware')(compiler, {
-  noInfo: true,
-  publicPath: config.output.publicPath
+    noInfo: true,
+    publicPath: config.output.publicPath,
 }));
 
 app.use(require('webpack-hot-middleware')(compiler));
 
-app.get('*', function(req, res) {
-  res.sendFile(path.join(__dirname, '/public/index.html'));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
-app.listen(3000, function(err) {
-  if (err) {
-    console.log(err);
-    return;
-  }
+app.listen(3000, (err) => {
+    if (err) {
+        console.log(err);
+        return;
+    }
 
-  console.log('Listening at http://localhost:3000');
+    console.log('Listening at http://localhost:3000');
 });
